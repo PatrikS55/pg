599e56a67f532ffd1d0d1adc533d8b4230ed20a1
PatrikS55 <cooltalongod@gmail.com>
2025-10-02 09:18:57 +0000

    idk

---
+++ cvičení1.py
@@ -0,0 +1,11 @@
+
+def delitelne_beze_zbytku_3(cislo):
+    return False
+
+
+
+if __name__ == "__main__":
+    if delitelne_beze_zbytku_3(3):
+        print("ano")
+    else:
+        print("ne")
---
+++ first.py
@@ -0,0 +1,10 @@
+
+def sudy_nebo_lichy(cislo):
+ cislo = int(input("vložte číslo"))
+
+    if cislo % 2 == 0:
+        print("Číslo je sudé")
+    else:
+        print("číslo je liché")
+
+
