6d2a871989ffa250a955f37bc9f3fb008bef3e6d
PatrikS55 <cooltalongod@gmail.com>
2025-10-07 08:39:54 +0000

    .

--- first.py
+++ first.py
@@ -2,7 +2,7 @@ def sudy_nebo_lichy(cislo):
     if cislo % 2 == 0:
         print("Číslo",cislo, "je sudé")
     else:
-        print(f"Číslo {cislo} je liché")
+        print(f"Číslo" ,cislo, "je liché")
 
 
 if __name__ == "__main__":
