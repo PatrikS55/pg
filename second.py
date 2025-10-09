f739c110130286cb321291e3e6d4bad7010c72b6
PatrikS55 <cooltalongod@gmail.com>
2025-10-09 10:27:51 +0000

    .

--- second.py
+++ second.py
@@ -1,11 +1,30 @@
 def cislo_text(cislo):
-    # funkce zkonvertuje cislo do jeho textove reprezentace
+    # funkce zkonvertuje číslo do jeho textové reprezentace (0–100)
     # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
-    x = ["jedna", "dva", "tři", "čtyři", "pět", "šest", "sedm", "osm", "devět", "deset"]
-    y = ["dvacet", "třicet", "čtyřicet", "padesát", "šedesát", "sedmdesát", "osmdesát", "devadesát", "sto"]
+    x = ["nula", "jedna", "dva", "tři", "čtyři", "pět", "šest", "sedm", "osm", "devět"]
+    y = ["dvacet", "třicet", "čtyřicet", "padesát", "šedesát", "sedmdesát", "osmdesát", "devadesát"]
+    z = ["deset", "jedenáct", "dvanáct", "třináct", "čtrnáct", "patnáct", "šestnáct", "sedmnáct", "osmnáct", "devatenáct"]
+
     
 
+    if cislo <= 9:
+        return x[cislo]
+    elif 10 <= cislo <= 19:
+        return z[cislo - 10]
+    elif cislo == 100:
+        return "sto"
+    else:
+        a = cislo // 10
+        b = cislo % 10
+        if b == 0:
+            return y[a - 2]
+        else:
+            return y[a - 2] + " " + x[b]
+
+
+
+
 if __name__ == "__main__":
-    cislo = input("Zadej číslo: ")
+    cislo = int(input("Zadej číslo: "))
     text = cislo_text(cislo)
     print(text)
