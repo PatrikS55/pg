3c8e63b20f568f3714193c352f3f634c9fe54acc
PatrikS55 <cooltalongod@gmail.com>
2025-11-12 08:39:51 +0000

    jh

--- fourth.py
+++ fourth.py
@@ -1,52 +1,113 @@
-
-def pesec(start, cil, obsazene):
+def pesec1(start, cil, obsazene):
     r, s = start
     cr, cs = cil
-    obsazene = {(2, 2), (8, 2), (3, 3), (5, 4), (8, 3), (8, 8), (6, 3), (1, 4)}
+
     if cs != s:
         return False
 
+    
     if cil in obsazene:
         return False
 
+    
     if cr == r + 1:
         return True
 
-    if r == 2 and cr == r + 2:
-        if (r + 1, s) not in obsazene:
-            return True
+
+    if r == 2 and cr == r + 2 and (r + 1, s) not in obsazene:
+        return True
 
     return False
 
-def strelec(start, cil, obsazene):
+def strelec1(start, cil, obsazene):
     r, s = start
     cr, cs = cil
-    obsazene = {(2, 2), (8, 2), (3, 3), (5, 4), (8, 3), (8, 8), (6, 3), (1, 4)}
-    
+
+    if abs(cr - r) != abs(cs - s):
+        return False
     if cil in obsazene:
         return False
+
     
+    dr = 1 if cr > r else -1
+    ds = 1 if cs > s else -1
+    rr, ss = r + dr, s + ds
+    while (rr, ss) != (cr, cs):
+        if (rr, ss) in obsazene:
+            return False
+        rr += dr
+        ss += ds
+    return True
 
-    if 
+
+def vez1(start, cil, obsazene):
+    r, s = start
+    cr, cs = cil
+    if not (r == cr or s == cs):
+        return False
+    if cil in obsazene:
+        return False
+
+    if r == cr:
+        step = 1 if cs > s else -1
+        for x in range(s + step, cs, step):
+            if (r, x) in obsazene:
+                return False
+    else:
+        step = 1 if cr > r else -1
+        for y in range(r + step, cr, step):
+            if (y, s) in obsazene:
+                return False
+    return True
 
 
+def jezdec1(start, cil, obsazene):
+    r, s = start
+    cr, cs = cil
+    if (abs(cr - r), abs(cs - s)) in [(1, 2), (2, 1)] and cil not in obsazene:
+        return True
+    return False
 
+
+def dama1(start, cil, obsazene):
+    # dáma = věž nebo střelec
+    return vez1(start, cil, obsazene) or strelec1(start, cil, obsazene)
+
+
+def kral1(start, cil, obsazene):
+    r, s = start
+    cr, cs = cil
+    if max(abs(cr - r), abs(cs - s)) == 1 and cil not in obsazene:
+        return True
+    return False
 
 
 def je_tah_mozny(figurka, cilova_pozice, obsazene_pozice):
     r, s = cilova_pozice
-
     if not (1 <= r <= 8 and 1 <= s <= 8):
         return False
 
-    if figurka["typ"] == "pěšec":
-        return pesec(figurka["pozice"], cilova_pozice, obsazene_pozice)
+    typ = figurka["typ"]
+    start = figurka["pozice"]
 
+    if typ == "pěšec":
+        return pesec1(start, cilova_pozice, obsazene_pozice)
+    elif typ == "střelec":
+        return strelec1(start, cilova_pozice, obsazene_pozice)
+    elif typ == "věž":
+        return vez1(start, cilova_pozice, obsazene_pozice)
+    elif typ == "jezdec":
+        return jezdec1(start, cilova_pozice, obsazene_pozice)
+    elif typ == "dáma":
+        return dama1(start, cilova_pozice, obsazene_pozice)
+    elif typ == "král":
+        return kral1(start, cilova_pozice, obsazene_pozice)
     return False
 
 
+
 if __name__ == "__main__":
-    pesec_fig = {"typ": "pěšec", "pozice": (2, 2)}
+    pesec = {"typ": "pěšec", "pozice": (2, 2)}
     jezdec = {"typ": "jezdec", "pozice": (3, 3)}
     vez = {"typ": "věž", "pozice": (8, 8)}
     strelec = {"typ": "střelec", "pozice": (6, 3)}
@@ -54,9 +115,9 @@ if __name__ == "__main__":
     kral = {"typ": "král", "pozice": (1, 4)}
     obsazene_pozice = {(2, 2), (8, 2), (3, 3), (5, 4), (8, 3), (8, 8), (6, 3), (1, 4)}
 
-    print(je_tah_mozny(pesec_fig, (3, 2), obsazene_pozice))
-    print(je_tah_mozny(pesec_fig, (4, 2), obsazene_pozice))
-    print(je_tah_mozny(pesec_fig, (1, 2), obsazene_pozice))
+    print(je_tah_mozny(pesec, (3, 2), obsazene_pozice))
+    print(je_tah_mozny(pesec, (4, 2), obsazene_pozice))
+    print(je_tah_mozny(pesec, (1, 2), obsazene_pozice))
 
     print(je_tah_mozny(jezdec, (4, 4), obsazene_pozice))
     print(je_tah_mozny(jezdec, (5, 4), obsazene_pozice))
--- prednaska2.py
+++ prednaska2.py
@@ -1,1 +1,23 @@
 
+def deleni(citatel, jmenovatel):
+    return citatel / jmenovatel
+
+if __name__ == "__main__":
+
+    try:
+
+        cislo1 = int(input("zadej čitatele: "))
+        while cislo1 is None:
+            try:
+
+                cislo2 = int(input("zadej jmenovatele: "))
+            except Exception:
+                print("zadej validní číslo")
+
+                výsledek = deleni(cislo1, cislo2)
+
+                print(výsledek)
+
+             except Exception:
+
+                print("v nasem programu nastala chyba") 
